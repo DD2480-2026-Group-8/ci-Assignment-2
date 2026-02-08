23c3508affe25237422f866a97b8c45809b66b39
nuggets4 <lzhangtwo@gmail.com>
2026-02-08 16:03:38 +0100

    feat: implementation for setCommitStatus
    refactor: commitstatus can just be a string

--- ci-app/src/main/java/com/group8/StatusToGithub.java
+++ ci-app/src/main/java/com/group8/StatusToGithub.java
@@ -2,6 +2,7 @@ package com.group8;
 import java.net.*;
 import java.net.http.*;
 
+
 public class StatusToGithub {
     private final String token;
     private final String owner;
@@ -30,15 +31,36 @@ public class StatusToGithub {
     }
 
     // accept (header), owner, repo, sha + STATES
-    public boolean setCommitStatus(String sha) {
-        String url = String.format("https://api.github.com/repos/%s/%s/commits/%s/status", owner, repo, sha);
-        return false;
+    // STATES: error, failure, pending, success
+    public boolean setCommitStatus(String sha, String state) {
+        try {
+            String url = String.format("https://api.github.com/repos/%s/%s/statuses/%s", owner, repo, sha); 
+            HttpClient client = HttpClient.newBuilder().build();
+
+            // JSON
+            String jsonString = "{'state': 'success'}";
+
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).header("accept", "application/json").header("authorization", "bearer " + token)
+                .header("content-type", "application/json").POST(HttpRequest.BodyPublishers.ofString(jsonString)).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println(response.statusCode());
+            if (response.statusCode() == 201) {
+                System.out.println("STATUS WAS SET");
+                return true;
+            } else {
+                System.out.println("STATUS WAS NOT SET");
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }  
 
     public static void main(String[] args) {
         StatusToGithub status = new StatusToGithub("DD2480-2026-Group-8", "ci-Assignment-2");
-
-        String url = status.getCommitStatus("lz-test-commit"); // TEST
+        boolean url = status.setCommitStatus("cbc1e57873979dbd97ccd532a01c85e138592f1a", "success");
         System.out.println(url);
     }
 }
