1f1cfa71e24ab2e082e774ed9eacb4cd0022916d
nuggets4 <lzhangtwo@gmail.com>
2026-02-08 15:11:27 +0100

    refactor: changed owner, repo, token to be instances

--- ci-app/src/main/java/com/group8/StatusToGithub.java
+++ ci-app/src/main/java/com/group8/StatusToGithub.java
@@ -3,14 +3,20 @@ import java.net.*;
 import java.net.http.*;
 
 public class StatusToGithub {
-    // private final String githubToken;
-    // private final String client;
+    private final String token;
+    private final String owner;
+    private final String repo;
 
+    public StatusToGithub(String owner, String repo) {
+        this.owner = owner;
+        this.repo = repo;
+        this.token = System.getenv("GH_TOKEN");
+    }
     // accept (header), owner, repo, ref
-    public String getCommitStatus(String owner, String repo, String ref) {
+
+    public String getCommitStatus(String ref) {
         try {
             String url = String.format("https://api.github.com/repos/%s/%s/commits/%s/status", owner, repo, ref);
-            String token = System.getenv("GH_TOKEN");
             HttpClient client = HttpClient.newBuilder().build();
             HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).header("accept", "application/json").header("authorization", "bearer " + token).build();
 
@@ -23,10 +29,12 @@ public class StatusToGithub {
         }
     }
 
-    public static void main(String[] args) {
-        StatusToGithub status = new StatusToGithub();
+    // public boolean setCommitStatus(String owner, String repo, )    
 
-        String url = status.getCommitStatus("DD2480-2026-Group-8", "ci-Assignment-2", "lz-test-commit"); // TEST
+    public static void main(String[] args) {
+        StatusToGithub status = new StatusToGithub("DD2480-2026-Group-8", "ci-Assignment-2");
+
+        String url = status.getCommitStatus("lz-test-commit"); // TEST
         System.out.println(url);
     }
 }
