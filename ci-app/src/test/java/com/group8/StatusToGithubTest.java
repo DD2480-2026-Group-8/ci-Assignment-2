4468774c68004bff399d32fe3a2fa397f5b7cfe6
JingzeGuo <jingzekuo@gmail.com>
2026-02-11 18:10:33 +0100

    test

--- ci-app/src/test/java/com/group8/StatusToGithubTest.java
+++ ci-app/src/test/java/com/group8/StatusToGithubTest.java
@@ -104,7 +104,6 @@ class StatusToGithubTest {
             }
         };
 
-
         StatusToGithub statusToGithub = new StatusToGithub(TEST_OWNER, TEST_REPO, testWrapper);
         String result = statusToGithub.getCommitStatus("abc123");
 
