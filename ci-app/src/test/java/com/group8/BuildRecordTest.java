09aa5ddce6a2e524ff821d64886ac65e10fa4708
JingzeGuo <jingzekuo@gmail.com>
2026-02-11 21:47:58 +0100

    test

--- ci-app/src/test/java/com/group8/BuildRecordTest.java
+++ ci-app/src/test/java/com/group8/BuildRecordTest.java
@@ -25,7 +25,6 @@ public class BuildRecordTest {
         // make it wrong
         //assertEquals(status, "FAILURE");
 
-
         String expectedDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(timestamp));
         assertEquals(expectedDate, json.getString("date"));
 
